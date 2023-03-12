1c0014a7242a2bc33f0facd90d64cecc81461d4d
Андрей <chesnokov20013@gmail.com>
2023-03-12 13:46:40 +0300

    refactoring

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,29 +1,10 @@
 package org.project;
 
-import jssc.SerialPortException;
-import org.project.services.fileFormatter.DataFormatter;
-import org.project.services.uart.UartReader;
-import org.project.services.uart.UartSender;
-
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import org.project.services.GUIMainForm;
 
 public class Main {
-    private static final String UART_PORT = "COM1";
-
-    public static void main(String[] args) throws FileNotFoundException, SerialPortException {
-        DataFormatter formatter = new DataFormatter();
-        UartSender sender = new UartSender(UART_PORT);
-
-        sender.send(formatter.format("/Users/IdeaProjects/kids_project/test.txt"));
-
-        UartReader reader = new UartReader(UART_PORT);
-
-        ArrayList<Double> results = reader.read();
-
-        for (Double s: results) {
-            System.out.println(s);
-        }
-
+    public static void main(String[] args) {
+        GUIMainForm guiMainForm = new GUIMainForm();
+        guiMainForm.mainGUI();
     }
 }
---
+++ src/main/java/org/project/services/GUIMainForm.java
@@ -0,0 +1,139 @@
+package org.project.services;
+
+import jssc.SerialPortException;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.FileNotFoundException;
+import org.project.services.fileFormatter.DataFormatter;
+import org.project.services.uart.UartReader;
+import org.project.services.uart.UartSender;
+import java.util.ArrayList;
+
+public class GUIMainForm extends JFrame {
+    private JPanel mainPanel;
+    private JPanel chooserFilesPanel;
+    private JPanel chFileNamePanel;
+    private JTextField textField1;
+    private JPanel chButtonPanel;
+    private JButton button1;
+    private JPanel chLabelPanel;
+    private JPanel ParametersPanel;
+    private JPanel radioPanel;
+    private JRadioButton bandPassRadioButton;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JPanel showParameters;
+    private JPanel outputParameters;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPanel footer;
+    private JButton startButton;
+    private final JFileChooser fc = new JFileChooser();
+    private int result;
+
+    public void mainGUI() {
+        JFrame frame = new JFrame("Microwave sensors(resonant)");
+        frame.setLocation(700,350);
+        frame.setContentPane(new GUIMainForm().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Object[] options = { "Да", "Нет" };
+                int n = JOptionPane
+                        .showOptionDialog(e.getWindow(), "Закрыть окно?",
+                                "Подтверждение выхода", JOptionPane.YES_NO_OPTION,
+                                JOptionPane.ERROR_MESSAGE, null, options,
+                                options[0]);
+                if (n == 0) {
+                    e.getWindow().setVisible(false);
+                    System.exit(0);
+                }
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+    }
+
+    public GUIMainForm() {
+        button1.addActionListener(e -> {
+            fc.setDialogTitle("Выбор текстового файла");
+            fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            result = fc.showOpenDialog(GUIMainForm.this);
+        });
+
+        button1.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (result == JFileChooser.APPROVE_OPTION ) {
+                    textField1.setText(fc.getName(fc.getSelectedFile()));
+                }
+            }
+        });
+
+        startButton.addActionListener(new ActionListener() {
+            private static final String UART_PORT = "COM1";
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DataFormatter formatter = new DataFormatter();
+                UartSender sender = new UartSender(UART_PORT);
+
+                try {
+                    sender.send(formatter.format("C:\\Users\\Андрей\\Desktop\\FQW\\FQW\\test.txt"));
+                } catch (SerialPortException | FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                UartReader reader = new UartReader(UART_PORT);
+
+                ArrayList<Double> results = null;
+                try {
+                    results = reader.read();
+                } catch (SerialPortException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                for (Double s: results) {
+                    System.out.println(s);
+                }
+            }
+        });
+    }
+}
+
